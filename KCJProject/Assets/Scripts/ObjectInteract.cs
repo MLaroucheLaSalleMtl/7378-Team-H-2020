472da378abec5f07813a00a708cf3b2f18fe13e4
sacripantas <caue_pizzol@hotmail.com>
2020-02-04 12:15:32 -0500

    End of 1st week - 1st sprint
    04/02 - End of the First week - Sprint 1
    + Test Scene with Terrain
    + Placeholder objects for scaling
    + FPS controls using new input system
    + Interactable intens scripting
    + Crosshair (UI)
    + Raycast Handler for FPS

---
+++ KCJProject/Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectInteract : MonoBehaviour
+{
+    private GameObject raycastedObject;
+    private GameManager code; // Reference to Game Manager for later implementation of object utility
+    private bool interact = false;
+
+    [SerializeField] private int rayLength = 2; // I made it a serialize field in order to easily manipulate it with the editor
+    [SerializeField] private LayerMask layerMaskInteract; // I created a layer for interactable objects in the editor
+    
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            interact = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        code = GameManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, forward, out hit, rayLength, layerMaskInteract)) //The layermask has no use here, for now
+        {
+            raycastedObject = hit.collider.gameObject;
+
+            if (interact == true) 
+            {
+                Debug.Log("You have interacted with an object");
+                Destroy(raycastedObject);
+                interact = false;
+            }
+        }
+    }
+}
